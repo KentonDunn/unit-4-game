2476c593aab3f45bb592efe62cbee7ed2b21484a
KentonDunn <kenton.dunn@gmail.com>
2018-04-04 22:00:08 -0400

    building folders and added some pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+//randomNumber div
+            //this random number should be between 19-120
+        
+//Wins and losses div   
+    //possibly use HTML solution
+
+
+//crystal buttons div (B1, B2, B3, B4)
+        //each button will have a random function assigned
+        //each random crystal button will generate a number between 1-12 
+    
+//your total score is 
+
+
+//results div 
+// this will add the random number generated for B1, B2, B3, B4
+
+    //when the number in the results div equals the randomNumber
+     //the wins will go up. 
+
+     //if the number in the results div becomes larger than the randomNumber
+
+     var youlikeMeat = true;
+//this will evaluate the value of this variable.  if it's true, it will do the function
+     if (youlikeMeat){
+        document.write("here is the meaty menu...");
+
+     }
